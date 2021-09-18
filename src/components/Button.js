da027a8c246d655837e816bde36cc2379bc8152f
Rennlad <rennickman@gmail.com>
2021-09-18 19:25:25 +0100

    home page and video

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Button.css';
 import { Link } from "react-router-dom";
 
+
 // Default values for the style and size of the button
 const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
@@ -10,7 +11,7 @@ const SIZES = ['btn--medium', 'btn--large'];
 
 export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
     
-    // Check for style and size values, use default if no values found
+    // Check for style and size values, use default if value hasnt been set
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
